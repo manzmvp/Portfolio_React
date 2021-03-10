6d9070366c4eb4d73f2849f6068a9b9a8bb92248
manzmvp <manz442001@gmail.com>
2021-03-10 07:24:31 +0530

    HashRouter

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { HashRouter as Router, Route } from 'react-router-dom'
 import Navbar from './components/Navbar';
 import Home from './contents/Home';
 import About from './contents/About';
